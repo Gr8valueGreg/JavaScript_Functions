175e38c0c9a749f66c362aef466e790bd5f80453
Justin Millard <justin0499@hotmail.com>
2025-11-06 19:36:33 -0500

    Made corrections to all exercises

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 console.log("Hello World!\n==========\n");
+console.log("EXERCISE 1:\n==========\n");
 
 function printOdds(count) {
     if (count == 0) {
@@ -19,20 +20,24 @@ function printOdds(count) {
 printOdds(10);
 printOdds(100);
 
-console.log("EXERCISE 1:\n==========\n");
+console.log("EXERCISE 2:\n==========\n");
 
-    if (typeof userName !== String) {
-        console.log("Incorrect input type please type your username.")
-    }
-    if (typeof age !== Number) {
-        console.log("Incorrect input type please type your age as an interger.")
-    }
+
 
 
 function checkAge(userName, age) {
     let aboveSixteen = `Congrats ${userName}, You can Drive!`;
     let belowSixteen = `Sorry ${userName}, but you need to wait until you're 16`;
 
+    if (typeof userName !== "string") {
+        console.log("Incorrect input type please type your username.");
+        return;
+    }
+    if (typeof age !== "number") {
+        console.log("Incorrect input type please type your age as an integer.");
+        return;
+    }  
+    
     if (age < 16) {
         console.log(belowSixteen);
     } else {
@@ -43,15 +48,18 @@ function checkAge(userName, age) {
 checkAge("Bill", 4);
 checkAge("Joe", 16);
 checkAge("Steve", 26);
+checkAge(0, "Greg")
+checkAge(10, 90)
+checkAge("John", "John")
 
-console.log("EXERCISE 2:\n==========\n");
+console.log("EXERCISE 3:\n==========\n");
 
 function plot(xCoord, yCoord) {
     if (xCoord == 0 && yCoord == 0) {
         console.log(`(${xCoord}, ${yCoord}) is at the origin`);
-    } else if (xCoord == 0) {
-        console.log(`(${xCoord}, ${yCoord}) is on the x-axis`);
     } else if (yCoord == 0) {
+        console.log(`(${xCoord}, ${yCoord}) is on the x-axis`);
+    } else if (xCoord == 0) {
         console.log(`(${xCoord}, ${yCoord}) is on the y-axis`);
     }
     if (xCoord >= 1 && yCoord >= 1) {
@@ -77,16 +85,17 @@ plot(-5, -5);
 plot(5, -5);
 
 
-console.log("EXERCISE 3:\n==========\n");
+console.log("EXERCISE 4:\n==========\n");
 
 
 function triangle(a, b, c) {
-    if (a + b < c && a + c < b && b + c < b) {
+    if (a + b <= c || a + c <= b || b + c <= a) {
         console.log(`Sides ${a}, ${b}, ${c} make an invalid triangle`)
+        return;
     }
-    if (a == b && b == c) {
+    if (a === b && b === c) {
         console.log(`Sides ${a}, ${b}, ${c} make an equilateral triangle`)
-    } else if (a == b || b == c || a == c) {
+    } else if (a === b || b === c || a === c) {
         console.log(`Sides ${a}, ${b}, ${c} make an isosceles triangle`)
     } else {
         console.log(`Sides ${a}, ${b}, ${c} make a scalene triangle`)
@@ -99,36 +108,45 @@ triangle(1, 2, 2);
 triangle(1, 2, 3);
 
 
-console.log("EXERCISE 4:\n==========\n");
+console.log("EXERCISE 5:\n==========\n");
 
 
 function dataUsage(planLimit, day, usage) {
     let planLength = 30;
     let remainingDays = planLength - day;
+    let recommendedDailyUse = planLimit / planLength;
     let actualDailyUse = usage / day;
-    let projectedDailyUse = planLimit / planLength;
-    let projectedUsage = (actualDailyUse * planLength) - planLimit;
-    let usageBudget = (planLimit - usage) / (planLength - day);
+    let projectedUsage = ((remainingDays * actualDailyUse) + usage) - planLimit;
+    let remainingData = planLimit - usage
+    let usageBudget = remainingData / remainingDays;
     let statusMsg = ""
+    let usageMsg = ""
 
-    if (actualDailyUse > projectedDailyUse) {
+    if (actualDailyUse > recommendedDailyUse) {
         statusMsg = "EXCEEDING";
-    } else if (actualDailyUse < projectedDailyUse) {
+    } else if (actualDailyUse < recommendedDailyUse) {
         statusMsg = "UNDER";
     } else {
         statusMsg = "AT";
     }
+
+    if (projectedUsage > 0) {
+        usageMsg = "OVER"
+    } else if (projectedUsage < 0) {
+        usageMsg = "UNDER"
+    } else {
+        usageMsg = ", EQUAL TO"
+    }
+
     console.log(`${day} day(s) used, ${remainingDays} day(s) remaining
-    Average daily use: ${projectedDailyUse.toFixed(2)} GB/day.
+    Average daily use: ${recommendedDailyUse.toFixed(3)} GB/day.
     You are ${statusMsg} your average daily use (${actualDailyUse.toFixed(2)} GB/day),
     continuing this usage, you'll end up using
-    ${projectedUsage.toFixed(2)} GB from your data limit.
+    ${Math.abs(projectedUsage.toFixed(1))} GB ${usageMsg} your data limit.
     To stay below your data plan, use no more than ${usageBudget.toFixed(2)} GB/day.`);
 }
 
 
 dataUsage(100, 15, 56);
 dataUsage(30, 20, 15);
-
-
-console.log("EXERCISE 5:\n==========\n");
+dataUsage(50, 10, 25);
